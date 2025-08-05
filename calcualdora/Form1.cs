b7954860e27a35eb3b05ab898c13d65739392d2c
LAPTOP-JRUMML2J\ronal <ronaldbejarano147>
2025-08-05 10:59:13 -0500

    Agregar archivos de proyecto.

---
+++ calcualdora/Form1.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace calcualdora
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            comboBox1.SelectedItem = "+";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double contenido = double.Parse(textBox1.Text);
+            double contenido2 = double.Parse(textBox2.Text);
+
+
+            double num1 = contenido;
+            double num2 = contenido2;
+
+            string seleccion = comboBox1.SelectedItem.ToString();
+
+            double res = 0;
+
+            switch (seleccion)
+            {
+                case "+":
+                    {
+                        res = num1 + num2;
+                        break;
+                    }
+
+                case "-":
+                    {
+                        res = num1 - num2;
+                        break;
+                    }
+                case "*":
+                    {
+                        res = num1 * num2;
+                        break;
+                    }
+                case "/":
+                    {
+                        res = num1 / num2;
+                        break;
+                    }
+
+            }
+            string final = res.ToString();
+            MessageBox.Show(final);
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
